7b9d628edf87f4d6f57d6dcfb25b4d0c4d112f5e
P17205320 <P17205320@my365.dmu.ac.uk>
2017-11-05 21:36:52 +0000

    Added basic animation
    Using Web Animations API

--- js/script.js
+++ js/script.js
@@ -3,16 +3,23 @@
   console.log(mainMenu);
   let subMenu = document.getElementById("submenu");
   console.log(subMenu);
-function subMenuAnimate() {
-  let test = this.getAttribute("menuItem");
 
-}
 mainMenu.addEventListener("click",  function(e)
 {
+  subMenu.innerHTML = "";
   if (e.target.dataset.menuitem === "prevWork") {
-    subMenu.innerHTML = "<a href = '#'> Previous Work</a>"
+    subMenu.innerHTML = "<a href = '#'> Level 2 Interactive Media</a>" +
+        "<a href = '#'> Level 3 Interactive Media</a>" +
+        "<a href = '#'> Level 3 IT</a>" +
+        "<a href = '#'> HND Computing</a>"
+
+  } else {
+
   }
+  subMenu.animate({ transform: [ 'scaleY(0)', 'scaleY(1)' ] },
+      { duration: 100, iterations: 1 });
   subMenu.style.display = "Block";
+
 });
 
 }());
