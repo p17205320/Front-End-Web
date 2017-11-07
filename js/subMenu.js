f3f5c2fc9f6bd3c6caa3b39edfcaccefc004d703
P17205320 <P17205320@my365.dmu.ac.uk>
2017-11-07 19:23:44 +0000

    Added basic text to about me page

--- js/subMenu.js
+++ js/subMenu.js
@@ -15,7 +15,15 @@ mainMenu.addEventListener("click",  function(e)
 
   } else if (e.target.dataset.menuitem === "aboutMe") {
     mainContent.animate([ { opacity: 1 }, { opacity: 0 } ], 500);
-        mainContent.innerHTML = "<p>Testing testing 123</p>"
+        mainContent.innerHTML = "<section>\n" +
+            "  <h1>About Me</h1>\n" +
+            "  <figure>\n" +
+            "    <img src=\"Media/About_Me/me_hnd_grad.jpg\">\n" +
+            "    <figcaption>Me at my graduation for HND Computing and Systems Development. 2nd from the right at top</figcaption>\n" +
+            "  </figure>\n" +
+            "  <p> My name is Joshua. I am a third year student at <a href = \"http://www.dmu.ac.uk/\">De Montfort University</a> studying <a href = \"http://www.dmu.ac.uk/study/courses/undergraduate-courses/computing-bsc/computing-bsc-degree.aspx\">BSC Computing</a>.\n" +
+            "    I was previously studing at <a href = \"http://www.stamford.ac.uk/\">New College Stamford</a> where i studied Interactive Media and Computing</p>\n" +
+            "</section>";
     mainContent.animate([ { opacity: 0 }, { opacity: 1 } ], 500);
   } else {
 
