cc3c65bd716d47c231dccdab430d7596b394abfa
P17205320 <P17205320@my365.dmu.ac.uk>
2017-11-09 11:58:22 +0000

    Created button set for Level 3 Media

--- js/subMenu.js
+++ js/subMenu.js
@@ -6,13 +6,13 @@
   let mainContent = document.getElementById("mainContent");
 mainMenu.addEventListener("click",  function(e)
 {
-  subMenu.innerHTML = "";
   if (e.target.dataset.menuitem === "prevWork") {
     subMenu.innerHTML = "<table> <tr> " +
-        "<td href = '#'> Level 3 Interactive Media</td>" +
-        "<td href = '#'> Level 3 IT</td>" +
-        "<td href = '#'> HND Computing</td></tr></table>"
-
+        "<td href = '#' data-menuItem = 'level3Media'> Level 3 Interactive Media</td>" +
+        "<td href = '#' data-menuItem = 'level3IT'> Level 3 IT</td>" +
+        "<td href = '#' data-menuItem = 'HNDIT'> HND Computing</td></tr></table>"
+    subMenu.animate([ { opacity: 0 }, { opacity: 1 } ], 500);
+    subMenu.style.display = "Block";
   } else if (e.target.dataset.menuitem === "aboutMe") {
     mainContent.animate([ { opacity: 1 }, { opacity: 0 } ], 500);
         mainContent.innerHTML = "<section>\n" +
@@ -25,11 +25,36 @@ mainMenu.addEventListener("click",  function(e)
             "    I was previously studing at <a href = \"http://www.stamford.ac.uk/\">New College Stamford</a> where i studied Interactive Media and Computing</p>\n" +
             "</section>";
     mainContent.animate([ { opacity: 0 }, { opacity: 1 } ], 500);
+  } else if (e.target.dataset.menuitem === "level3Media") {
+    mainContent.animate([ { opacity: 1 }, { opacity: 0 } ], 500);
+      mainContent.innerHTML = "<div id =\"mainButtonContain\">\n" +
+          "  <h1>Interactive Media Level 3</h1>\n" +
+          "  <h2>Works</h2>\n" +
+          "  <table>\n" +
+          "    <tr href=\"#\" class = \"mainButton\">\n" +
+          "      <td>\n" +
+          "        <img src=\"Media/Level_3_Interactive_Media/Button/None.jpg\">\n" +
+          "      </td>\n" +
+          "      <td>\n" +
+          "        <h1>None</h1>\n" +
+          "      </td>\n" +
+          "    </tr>\n" +
+          "    <tr href=\"#\" class = \"mainButton\">\n" +
+          "      <td>\n" +
+          "        <img src=\"Media/Level_3_Interactive_Media/Button/Robot's%20Trip.jpg\">\n" +
+          "      </td>\n" +
+          "      <td>\n" +
+          "        <h1>Robot's Trip</h1>\n" +
+          "      </td>\n" +
+          "    </tr>\n" +
+          "  </table>\n" +
+          "</div>";
+    mainContent.animate([ { opacity: 0 }, { opacity: 1 } ], 500);
   } else {
 
   }
-  subMenu.animate([ { opacity: 0 }, { opacity: 1 } ], 500);
-  subMenu.style.display = "Block";
+
+
 
 });
 
