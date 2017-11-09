6d628adad1cbbe3f64e69a146734b1c2cf21007b
P17205320 <P17205320@my365.dmu.ac.uk>
2017-11-09 20:46:13 +0000

    Changed the way button sets are loaded
    Injecting the buttons using InnerHTML was causing event  listeners to not work because the DOM was mutated. Doing it this way mitigates that

--- js/subMenu.js
+++ js/subMenu.js
@@ -4,6 +4,8 @@
   let subMenu = document.getElementById("subMenu");
   console.log(subMenu);
   let mainContent = document.getElementById("mainContent");
+  console.log(mainContent);
+  let pageLoadContent = document.getElementById("pageLoad");
   mainMenu.addEventListener("click",  function(e)
   {
     if (e.target.dataset.menuitem === "prevWork") {
@@ -17,7 +19,9 @@
       subMenu.style.display = "Block";
     } else if (e.target.dataset.menuitem === "aboutMe") {
       mainContent.animate([ { opacity: 1 }, { opacity: 0 } ], 500);
-      mainContent.innerHTML = "<section>\n" +
+      eventKill();
+      hideAll();
+      pageLoadContent.innerHTML = "<section>\n" +
           "  <h1>About Me</h1>\n" +
           "  <figure>\n" +
           "    <img src=\"Media/About_Me/me_hnd_grad.jpg\">\n" +
@@ -29,132 +33,31 @@
       mainContent.animate([ { opacity: 0 }, { opacity: 1 } ], 500);
     } else if (e.target.dataset.menuitem === "level3Media") {
       mainContent.animate([ { opacity: 1 }, { opacity: 0 } ], 500);
-      mainContent.innerHTML = "<div id =\"mainButtonContain\">\n" +
-          "  <h1>Interactive Media Level 3</h1>\n" +
-          "  <h2>Works</h2>\n" +
-          "  <table>\n" +
-          "    <tr href=\"#\" data-button = \"none\" class = \"mainButton\">\n" +
-          "      <td>\n" +
-          "        <img src=\"Media/Level_3_Interactive_Media/Button/None.jpg\">\n" +
-          "      </td>\n" +
-          "      <td>\n" +
-          "        <h1>None</h1>\n" +
-          "      </td>\n" +
-          "    </tr>\n" +
-          "    <tr href=\"#\" data-button = \"robotsTrip\" class = \"mainButton\">\n" +
-          "      <td>\n" +
-          "        <img src=\"Media/Level_3_Interactive_Media/Button/Robot's%20Trip.jpg\">\n" +
-          "      </td>\n" +
-          "      <td>\n" +
-          "        <h1>Robot's Trip</h1>\n" +
-          "      </td>\n" +
-          "    </tr>\n" +
-          "  </table>\n" +
-          "</div>";
-      let mainButton = document.getElementsByClassName("mainButton");
+      eventKill();
+      hideAll();
+      document.getElementById("mainButtonContain1").style.display = "Block";
+      document.getElementById("mainButtonContain1").addEventListener("click", pageLoad, e);
       mainContent.animate([ { opacity: 0 }, { opacity: 1 } ], 500);
     }else if (e.target.dataset.menuitem === "level3IT") {
       mainContent.animate([ { opacity: 1 }, { opacity: 0 } ], 500);
-      mainContent.innerHTML = "<div id =\"mainButtonContain\">\n" +
-          "  <h1>Level 3 IT</h1>\n" +
-          "  <h2>Works</h2>\n" +
-          "  <table>\n" +
-          "    <tr href=\"#\" data-button = \"battleship\" class = \"mainButton\">\n" +
-          "      <td>\n" +
-          "        <img src=\"Media/Level_3_IT/Button/Battleship.jpg\">\n" +
-          "      </td>\n" +
-          "      <td>\n" +
-          "        <h1>Battleship</h1>\n" +
-          "      </td>\n" +
-          "    </tr>\n" +
-          "    <tr href=\"#\" data-button = \"monsta\" class = \"mainButton\">\n" +
-          "      <td>\n" +
-          "        <img src=\"Media/Level_3_IT/Button/Monsta.jpg\">\n" +
-          "      </td>\n" +
-          "      <td>\n" +
-          "        <h1>Monsta</h1>\n" +
-          "      </td>\n" +
-          "    </tr>\n" +
-          "    <tr href=\"#\" data-button = \"ncsVideo\" class = \"mainButton\">\n" +
-          "      <td>\n" +
-          "        <img src=\"Media/Level_3_IT/Button/NCS_Promo.jpg\">\n" +
-          "      </td>\n" +
-          "      <td>\n" +
-          "        <h1>New College Stamford Promo</h1>\n" +
-          "      </td>\n" +
-          "    </tr>\n" +
-          "    <tr href=\"#\" data-button = \"stoningCyprus\" class = \"mainButton\">\n" +
-          "      <td>\n" +
-          "        <img src=\"Media/Level_3_IT/Button/Stoning_Cyprus.jpg\">\n" +
-          "      </td>\n" +
-          "      <td>\n" +
-          "        <h1>Stoning Cyprus</h1>\n" +
-          "      </td>\n" +
-          "    </tr>\n" +
-          "  </table>\n" +
-          "</div>";
-      let mainButton = document.getElementsByClassName("mainButton");
+      eventKill();
+      hideAll();
+      document.getElementById("mainButtonContain2").style.display = "Block";
+      document.getElementById("mainButtonContain2").addEventListener("click", pageLoad, e);
       mainContent.animate([ { opacity: 0 }, { opacity: 1 } ], 500);
     }else if (e.target.dataset.menuitem === "HNDIT") {
       mainContent.animate([ { opacity: 1 }, { opacity: 0 } ], 500);
-      mainContent.innerHTML = "<div id =\"mainButtonContain\">\n" +
-          "  <h1>HND Computing</h1>\n" +
-          "  <h2>Works</h2>\n" +
-          "  <table>\n" +
-          "    <tr href=\"#\" data-button = \"cicVideo\" class = \"mainButton\">\n" +
-          "      <td>\n" +
-          "        <img src=\"Media/HND_Computing/Button/CIC_Video.jpg\">\n" +
-          "      </td>\n" +
-          "      <td>\n" +
-          "        <h1>CIC Video</h1>\n" +
-          "      </td>\n" +
-          "    </tr>\n" +
-          "    <tr href=\"#\" data-button = \"stamfordVideo\" class = \"mainButton\">\n" +
-          "      <td>\n" +
-          "        <img src=\"Media/HND_Computing/Button/Stamford_History.jpg\">\n" +
-          "      </td>\n" +
-          "      <td>\n" +
-          "        <h1>History of Stamford</h1>\n" +
-          "      </td>\n" +
-          "    </tr>\n" +
-          "    <tr href=\"#\" data-button = \"snek\" class = \"mainButton\">\n" +
-          "      <td>\n" +
-          "        <img src=\"Media/HND_Computing/Button/Snek.png\">\n" +
-          "      </td>\n" +
-          "      <td>\n" +
-          "        <h1>Snek</h1>\n" +
-          "      </td>\n" +
-          "    </tr>\n" +
-          "    <tr href=\"#\" data-button = \"xonarSystems\" class = \"mainButton\">\n" +
-          "      <td>\n" +
-          "        <img src=\"Media/HND_Computing/Button/Xonar_Systems.jpg\">\n" +
-          "      </td>\n" +
-          "      <td>\n" +
-          "        <h1>Xonar Systems</h1>\n" +
-          "      </td>\n" +
-          "    </tr>\n" +
-          "  </table>\n" +
-          "</div>";
-      let mainButton = document.getElementsByClassName("mainButton");
+      eventKill();
+      hideAll();
+      document.getElementById("mainButtonContain3").style.display = "Block";
+      document.getElementById("mainButtonContain3").addEventListener("click", pageLoad, e);
       mainContent.animate([ { opacity: 0 }, { opacity: 1 } ], 500);
     }else if (e.target.dataset.menuitem === "other") {
       mainContent.animate([ { opacity: 1 }, { opacity: 0 } ], 500);
-      mainContent.innerHTML = "<div id =\"mainButtonContain\">\n" +
-          "  <h1>Other</h1>\n" +
-          "  <h2>Works</h2>\n" +
-          "  <table>\n" +
-          "    <tr href=\"#\" data-button = \"grantham\" class = \"mainButton\">\n" +
-          "      <td>\n" +
-          "        <img src=\"Media/Other/Button/Grantham.jpg\">\n" +
-          "      </td>\n" +
-          "      <td>\n" +
-          "        <h1>A documentary we did about shops in grantham</h1>\n" +
-          "      </td>\n" +
-          "    </tr>\n" +
-          "  </table>\n" +
-          "</div>";
-      let mainButton = document.getElementsByClassName("mainButton");
-      console.log(mainButton);
+      eventKill();
+      hideAll();
+      document.getElementById("mainButtonContain4").style.display = "Block";
+      document.getElementById("mainButtonContain4").addEventListener("click", pageLoad, e);
       mainContent.animate([ { opacity: 0 }, { opacity: 1 } ], 500);
     } else {
     }
@@ -163,56 +66,78 @@
 
 
 
+
     });
-  mainButton.addEventListener("click",  function(e) {
-    if (e.target.dataset.button === "none") {
-      mainContent.animate([ { opacity: 1 }, { opacity: 0 } ], 500);
-      mainContent.innerHTML = "";
-      mainContent.animate([ { opacity: 0 }, { opacity: 1 } ], 500);
-    } else if (e.target.dataset.button === "robotsTrip") {
-      mainContent.animate([ { opacity: 1 }, { opacity: 0 } ], 500);
-      mainContent.innerHTML = "";
-      mainContent.animate([ { opacity: 0 }, { opacity: 1 } ], 500);
-    } else if (e.target.dataset.button === "battleship") {
-      mainContent.animate([ { opacity: 1 }, { opacity: 0 } ], 500);
-      mainContent.innerHTML = "";
-      mainContent.animate([ { opacity: 0 }, { opacity: 1 } ], 500);
-    } else if (e.target.dataset.button === "monsta") {
-      mainContent.animate([ { opacity: 1 }, { opacity: 0 } ], 500);
-      mainContent.innerHTML = "";
-      mainContent.animate([ { opacity: 0 }, { opacity: 1 } ], 500);
-    } else if (e.target.dataset.button === "ncsVideo") {
-      mainContent.animate([ { opacity: 1 }, { opacity: 0 } ], 500);
-      mainContent.innerHTML = "";
-      mainContent.animate([ { opacity: 0 }, { opacity: 1 } ], 500);
-    } else if (e.target.dataset.button === "stoningCyprus") {
-      mainContent.animate([ { opacity: 1 }, { opacity: 0 } ], 500);
-      mainContent.innerHTML = "";
-      mainContent.animate([ { opacity: 0 }, { opacity: 1 } ], 500);
-    } else if (e.target.dataset.button === "cicVideo") {
-      mainContent.animate([ { opacity: 1 }, { opacity: 0 } ], 500);
-      mainContent.innerHTML = "";
-      mainContent.animate([ { opacity: 0 }, { opacity: 1 } ], 500);
-    } else if (e.target.dataset.button === "stamfordVideo") {
-      mainContent.animate([ { opacity: 1 }, { opacity: 0 } ], 500);
-      mainContent.innerHTML = "";
-      mainContent.animate([ { opacity: 0 }, { opacity: 1 } ], 500);
-    } else if (e.target.dataset.button === "snek") {
-      mainContent.animate([ { opacity: 1 }, { opacity: 0 } ], 500);
-      mainContent.innerHTML = "";
-      mainContent.animate([ { opacity: 0 }, { opacity: 1 } ], 500);
-    } else if (e.target.dataset.button === "xonarSystems") {
-      mainContent.animate([ { opacity: 1 }, { opacity: 0 } ], 500);
-      mainContent.innerHTML = "";
-      mainContent.animate([ { opacity: 0 }, { opacity: 1 } ], 500);
-    } else if (e.target.dataset.button === "grantham") {
-      mainContent.animate([ { opacity: 1 }, { opacity: 0 } ], 500);
-      mainContent.innerHTML = "";
-      mainContent.animate([ { opacity: 0 }, { opacity: 1 } ], 500);
-    }
-    else {
 
-    }
+    function eventKill() {
+      document.getElementById("mainButtonContain1").removeEventListener("click", pageLoad);
+      document.getElementById("mainButtonContain2").removeEventListener("click", pageLoad);
+      document.getElementById("mainButtonContain3").removeEventListener("click", pageLoad);
+      document.getElementById("mainButtonContain4").removeEventListener("click", pageLoad);
+    };
 
-  });
+    function hideAll() {
+      document.getElementById("mainButtonContain2").style.display = "None";
+      document.getElementById("mainButtonContain3").style.display = "None";
+      document.getElementById("mainButtonContain1").style.display = "None";
+      document.getElementById("mainButtonContain4").style.display = "None";
+      pageLoadContent.innerHTML = "";
+    }
+    function pageLoad (e) {
+
+      if (e.target.dataset.button === "none") {
+        mainContent.animate([ { opacity: 1 }, { opacity: 0 } ], 500);
+        hideAll();
+        pageLoadContent.innerHTML = "<video>\n" +
+            "  <source src=\"Media/Level_3_Interactive_Media/Video/None.webm\">\n" +
+            "</video>";
+        mainContent.animate([ { opacity: 0 }, { opacity: 1 } ], 500);
+      } else if (e.target.dataset.button === "robotsTrip") {
+        mainContent.animate([ { opacity: 1 }, { opacity: 0 } ], 500);
+        mainContent.innerHTML = "";
+        mainContent.animate([ { opacity: 0 }, { opacity: 1 } ], 500);
+      } else if (e.target.dataset.button === "battleship") {
+        mainContent.animate([ { opacity: 1 }, { opacity: 0 } ], 500);
+        mainContent.innerHTML = "";
+        mainContent.animate([ { opacity: 0 }, { opacity: 1 } ], 500);
+      } else if (e.target.dataset.button === "monsta") {
+        mainContent.animate([ { opacity: 1 }, { opacity: 0 } ], 500);
+        mainContent.innerHTML = "";
+        mainContent.animate([ { opacity: 0 }, { opacity: 1 } ], 500);
+      } else if (e.target.dataset.button === "ncsVideo") {
+        mainContent.animate([ { opacity: 1 }, { opacity: 0 } ], 500);
+        mainContent.innerHTML = "";
+        mainContent.animate([ { opacity: 0 }, { opacity: 1 } ], 500);
+      } else if (e.target.dataset.button === "stoningCyprus") {
+        mainContent.animate([ { opacity: 1 }, { opacity: 0 } ], 500);
+        mainContent.innerHTML = "";
+        mainContent.animate([ { opacity: 0 }, { opacity: 1 } ], 500);
+      } else if (e.target.dataset.button === "cicVideo") {
+        mainContent.animate([ { opacity: 1 }, { opacity: 0 } ], 500);
+        mainContent.innerHTML = "";
+        mainContent.animate([ { opacity: 0 }, { opacity: 1 } ], 500);
+      } else if (e.target.dataset.button === "stamfordVideo") {
+        mainContent.animate([ { opacity: 1 }, { opacity: 0 } ], 500);
+        mainContent.innerHTML = "";
+        mainContent.animate([ { opacity: 0 }, { opacity: 1 } ], 500);
+      } else if (e.target.dataset.button === "snek") {
+        mainContent.animate([ { opacity: 1 }, { opacity: 0 } ], 500);
+        mainContent.innerHTML = "";
+        mainContent.animate([ { opacity: 0 }, { opacity: 1 } ], 500);
+      } else if (e.target.dataset.button === "xonarSystems") {
+        mainContent.animate([ { opacity: 1 }, { opacity: 0 } ], 500);
+        mainContent.innerHTML = "";
+        mainContent.animate([ { opacity: 0 }, { opacity: 1 } ], 500);
+      } else if (e.target.dataset.button === "grantham") {
+        mainContent.animate([ { opacity: 1 }, { opacity: 0 } ], 500);
+        mainContent.innerHTML = "";
+        mainContent.animate([ { opacity: 0 }, { opacity: 1 } ], 500);
+      }
+      else {
+
+      }
+    };
+
+
+
 }());
