06c21f41826f5e6b9b78e0065fbeb4141ba2e12f
P17205320 <P17205320@my365.dmu.ac.uk>
2017-11-11 16:57:10 +0000

    Added rest of pages and text to homepage
    Website is now mostly complete

--- js/subMenu.js
+++ js/subMenu.js
@@ -59,7 +59,12 @@
       document.getElementById("mainButtonContain4").style.display = "Block";
       document.getElementById("mainButtonContain4").addEventListener("click", pageLoad, e);
       mainContent.animate([ { opacity: 0 }, { opacity: 1 } ], 500);
-    } else {
+    } else if (e.target.dataset.menuitem === "homePage") {
+      mainContent.animate([ { opacity: 1 }, { opacity: 0 } ], 500);
+      hideAll();
+      pageLoadContent.innerHTML = "<p>Hello and welcome to my portfolio page. Please use the buttons above to navigate the site and see all the previous projects i've done for courses i have been able to find since 2009</p>\n" +
+          "  <p>This collection isn't complete but the missing stuff is really awful so you are not missing out on much (Be glad you don't see the first website i ever made. Using Dreamweaver <sub>shudders</sub>)</p>";
+      mainContent.animate([ { opacity: 0 }, { opacity: 1 } ], 500);
     }
 
 
@@ -189,33 +194,87 @@
       } else if (e.target.dataset.button === "cicVideo") {
         mainContent.animate([ { opacity: 1 }, { opacity: 0 } ], 500);
         hideAll();
-        pageLoadContent.innerHTML = "";
+        pageLoadContent.innerHTML = "<article>\n" +
+            "  <h1>CiC Video Site</h1>\n" +
+            "  <figure>\n" +
+            "    <img src = \"https://portfolio.joshua-s.website/Media/HND_Computing/Image/cic_video/1.png\" alt = \"Screenshot of CIC Video Site\">\n" +
+            "    <figcaption><a href = \"https://cic-video.joshua-s.website\">Live Version</a></figcaption>\n" +
+            "  </figure>\n" +
+            "  <ul>\n" +
+            "    <li>IMDB like site</li>\n" +
+            "    <li>First experience with PHP & MySQL</li>\n" +
+            "  </ul>\n" +
+            "</article>";
         mainContent.animate([ { opacity: 0 }, { opacity: 1 } ], 500);
       } else if (e.target.dataset.button === "stamfordVideo") {
         mainContent.animate([ { opacity: 1 }, { opacity: 0 } ], 500);
         hideAll();
-        pageLoadContent.innerHTML = "";
+        pageLoadContent.innerHTML = "<article>\n" +
+            "  <h1>History of Stamford</h1>\n" +
+            "  <video controls>\n" +
+            "    <source src=\"https://portfolio.joshua-s.website/Media/HND_Computing/Video/Stamford_History.webm\">\n" +
+            "  </video>\n" +
+            "  <ul>\n" +
+            "    <li>Created to showcase the history of Stamford</li>\n" +
+            "    <li>Filmed on a phone and stabilized in post (We were supposed to get a proper camera but whatever)</li>\n" +
+            "  </ul>\n" +
+            "</article>";
         mainContent.animate([ { opacity: 0 }, { opacity: 1 } ], 500);
       } else if (e.target.dataset.button === "snek") {
         mainContent.animate([ { opacity: 1 }, { opacity: 0 } ], 500);
         hideAll();
-        pageLoadContent.innerHTML = "";
+        pageLoadContent.innerHTML = "<article>\n" +
+            "  <h1>Snek</h1>\n" +
+            "  <video controls>\n" +
+            "    <source src=\"https://portfolio.joshua-s.website/Media/HND_Computing/Video/Snek_Gameplay.webm\">\n" +
+            "  </video>\n" +
+            "  <ul>\n" +
+            "    <li>Created using C#</li>\n" +
+            "    <lI>First Experience with C# games dev</lI>\n" +
+            "  </ul>\n" +
+            "</article>";
         mainContent.animate([ { opacity: 0 }, { opacity: 1 } ], 500);
       } else if (e.target.dataset.button === "xonarSystems") {
         mainContent.animate([ { opacity: 1 }, { opacity: 0 } ], 500);
         hideAll();
-        pageLoadContent.innerHTML = "";
+        pageLoadContent.innerHTML = "<article>\n" +
+            "  <h1>Xonar Systems Site</h1>\n" +
+            "  <figure>\n" +
+            "    <img src = \"https://portfolio.joshua-s.website/Media/HND_Computing/Image/xonar_systems/1.png\" alt = \"Screenshot of Xonar Systems Site\">\n" +
+            "    <figcaption><a href = \"https://xonar-systems.joshua-s.website\">Live Version</a></figcaption>\n" +
+            "  </figure>\n" +
+            "  <ul>\n" +
+            "    <li>PC Configuator/Ordering Site</li>\n" +
+            "    <li>Created using PHP</li>\n" +
+            "    <li>Took 4 days to create</li>\n" +
+            "  </ul>\n" +
+            "</article>";
         mainContent.animate([ { opacity: 0 }, { opacity: 1 } ], 500);
       } else if (e.target.dataset.button === "grantham") {
         mainContent.animate([ { opacity: 1 }, { opacity: 0 } ], 500);
         hideAll();
-        pageLoadContent.innerHTML = "";
+        pageLoadContent.innerHTML = "<article>\n" +
+            "  <h1>A documentary we did about the shops in grantham</h1>\n" +
+            "  <video controls>\n" +
+            "    <source src=\"https://portfolio.joshua-s.website/Media/Other/Video/grantham_doc.webm\">\n" +
+            "  </video>\n" +
+            "  <ul>\n" +
+            "    <li>Extended/Directors cut made years after the original</li>\n" +
+            "    <li>Bad title because i forgot the original one</li>\n" +
+            "    <li>Original version made in 2009</li>\n" +
+            "    <li>Created for a documentary contest. Group that created the best documentary won something and was supposed to be featured in an article in the guardian or something</li>\n" +
+            "    <li>Audio is terrible because the backup director forgot to plug in the mic (I was ill on the timing of shooting)</li>\n" +
+            "    <li>Original edit by the backup director was soo bad i had to edit a new version on the day of submission</li>\n" +
+            "    <li>Original edit won the competition against the other 3 entries!</li>\n" +
+            "    <li><small>But only because it was the only entry the meet the entry criteria!</small></li>\n" +
+            "    <li>That article never happened surprisingly</li>\n" +
+            "    <li>I was somehow accepted onto Level 3 Interactive Media with just this in my portfolio</li>\n" +
+            "  </ul>\n" +
+            "</article>";
         mainContent.animate([ { opacity: 0 }, { opacity: 1 } ], 500);
       }
-      else {
 
-      }
-    };
+    }
 
 
 
