44f4e03f67b75e449bf31c1c131e924555402d70
P17205320 <P17205320@my365.dmu.ac.uk>
2017-12-02 18:55:57 +0000

    Finshed page for photography

--- js/photoLoad.js
+++ js/photoLoad.js
@@ -11,7 +11,12 @@
         document.removeEventListener("DOMNodeInserted", photo);
         console.log(document.getElementById("photoThumb"));
         document.getElementById("photoThumb").addEventListener("click", function(e) {
-          console.log(e);
+          console.log(e.target.dataset);
+
+          document.getElementById("photoFull").innerHTML = "<img src='https://portfolio.joshua-s.website/Media/Level_3_Interactive_Media/Image/photography/Full/"+ e.target.dataset.photo + ".JPG'>";
+          document.getElementById("photoFull").innerHTML += "<a href='https://portfolio.joshua-s.website/Media/Level_3_Interactive_Media/Image/photography/Full/"+ e.target.dataset.photo + ".JPG'> Full Image </a>";
+
+          window.scrollTo(0, 0);
 
         })
       };
