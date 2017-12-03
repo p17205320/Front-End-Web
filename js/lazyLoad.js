28432023b43d1992ffe89e5eb9bd10ef62d4d458
P17205320 <P17205320@my365.dmu.ac.uk>
2017-12-03 11:33:50 +0000

    Added lazy loading to rest of pages

--- js/lazyLoad.js
+++ js/lazyLoad.js
@@ -1,14 +1,17 @@
 function lazyLoad() {
-
+    //Solution taken from https://davidwalsh.name/lazyload-image-fade but modified to show a loading indicator also
+    //This functional loads all images with the data-src data attribute and sets the attribute to the src attribute and then removes the data attribute when the image is loaded. CSS is then used to fade in the image.
     [].forEach.call(document.querySelectorAll("img[data-src]"),    function(img) {
       img.setAttribute("src", img.getAttribute("data-src"));
 
       img.onload = function() {
 
         img.removeAttribute("data-src");
-        console.log(img.nextSibling);
-        if (img.nextSibling.nodeName = "P") {
-          img.nextSibling.remove();
+        // Put in to make the console stop moaning at me
+        //This is what i added to the function. It removes the loading indicator once the image is loaded
+        if (img.nextElementSibling.id = "loadingIndicate") {
+            img.nextElementSibling.remove();
+
         }
 
       };
