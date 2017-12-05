e57361b11e40208075a6cf3c85bc4038e284fe2f
P17205320 <P17205320@my365.dmu.ac.uk>
2017-12-05 21:34:20 +0000

    Fixed issue with xmlhttprequest function
    Other functions were firing as it was waiting for the state of the request to change meaning the page would display before content was loaded. This ensures that the page is shown only after the content is loaded

--- js/subMenu.js
+++ js/subMenu.js
@@ -16,107 +16,84 @@
         "  <button  data-menuItem = 'other'> Other</button>";
       subMenu.classList.add("transition");
     } else if (e.target.dataset.menuitem === "aboutMe") {
-      hideAll();
+      
       loadContent("Pages/other/aboutme.txt");
-      pageTransition();
     } else if (e.target.dataset.menuitem === "level3Media") {
       
-
-      hideAll();
       loadContent("Pages/buttons/level3media.txt");
-      pageTransition();
-
     }else if (e.target.dataset.menuitem === "level3IT") {
       
-
-      hideAll();
       loadContent("Pages/buttons/level3it.txt");
-      pageTransition();
-
     }else if (e.target.dataset.menuitem === "HNDIT") {
       
-
-      hideAll();
       loadContent("Pages/buttons/hndcomputing.txt");
-      pageTransition();
-
     }else if (e.target.dataset.menuitem === "other") {
       
-
-      hideAll();
       loadContent("Pages/buttons/other.txt");
-      pageTransition();
-
     } else if (e.target.dataset.menuitem === "homePage") {
       
-
-      hideAll();
       loadContent("Pages/other/homepage.txt");
-
-      pageTransition();
-
-      
     }
     if (e.target.dataset.button === "none") {
-      hideAll();
+      
       subMenuHide();
       loadContent("Pages/level3media/none.txt");
-      pageTransition();
+      
     } else if (e.target.dataset.button === "robotsTrip") {
-      hideAll();
+      
       subMenuHide();
       loadContent("Pages/level3media/robotstrip.txt");
-      pageTransition();
+      
     } else if (e.target.dataset.button === "battleship") {
-      hideAll();
+      
       subMenuHide();
       loadContent("Pages/level3it/battleship.txt");
-      pageTransition();
+      
     } else if (e.target.dataset.button === "monsta") {
-      hideAll();
+      
       subMenuHide();
       loadContent("Pages/level3it/monsta.txt");
-      pageTransition();
+      
     } else if (e.target.dataset.button === "ncsVideo") {
-      hideAll();
+      
       subMenuHide();
       loadContent("Pages/level3it/ncsvideo.txt");
-      pageTransition();
+      
     } else if (e.target.dataset.button === "stoningCyprus") {
-      hideAll();
+      
       subMenuHide();
       loadContent("Pages/level3it/stoningcyprus.txt");
-      pageTransition();
+      
     } else if (e.target.dataset.button === "cicVideo") {
-      hideAll();
+      
       subMenuHide();
       loadContent("Pages/hndcomputing/cicvideo.txt");
-      pageTransition();
+      
     } else if (e.target.dataset.button === "stamfordVideo") {
-      hideAll();
+      
       subMenuHide();
       loadContent("Pages/hndcomputing/stamfordhistory.txt");
-      pageTransition();
+      
     } else if (e.target.dataset.button === "snek") {
-      hideAll();
+      
       subMenuHide();
       loadContent("Pages/hndcomputing/snek.txt");
-      pageTransition();
+      
     } else if (e.target.dataset.button === "xonarSystems") {
-      hideAll();
+      
       subMenuHide();
       loadContent("Pages/hndcomputing/xonarsystems.txt");
-      pageTransition();
+      
     } else if (e.target.dataset.button === "grantham") {
-      hideAll();
+      
       subMenuHide();
       loadContent("Pages/other/grantham.txt");
-      pageTransition();
+      
     } else if (e.target.dataset.button === "photography") {
-      hideAll();
+      
       subMenuHide();
       loadContent("Pages/level3media.txt/photography.txt");
-      pageTransition();
+      
       document.addEventListener("DOMNodeInserted", photo);
 
     }
@@ -129,10 +106,12 @@
   });
 
   function loadContent(e) {
-    var xhttp = new XMLHttpRequest();
+    let xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
+        hideAll();
         mainContent.innerHTML = this.responseText;
+        pageTransition();
       }
     };
     xhttp.open("GET", e, true);
@@ -144,16 +123,13 @@
   function hideAll() {
     mainContent.style.display = "None";
     mainContent.innerHTML = "";
+    mainContent.classList.remove("transition");
+
   }
   
   function pageTransition() {
     mainContent.style.display = "Block";
     mainContent.classList.add("transition");
-  }
-  function pageLoad (e) {
-    pageTransition();
-
-
   }
 
 
