0bebc776c3e4f96b3e05306ab97c79970ea09a98
P17205320 <P17205320@my365.dmu.ac.uk>
2017-12-06 11:56:48 +0000

    Fixed issues where photoload was firing without the user pressing an image

--- js/photoLoad.js
+++ js/photoLoad.js
@@ -7,9 +7,11 @@
         document.getElementById("photoThumb").removeEventListener("click", function(e){});
         document.getElementById("photoThumb").addEventListener("click", function(e) {
           console.log(e.target.dataset);
+          if (e.target.dataset.photo !== undefined) {
+              document.getElementById("photoFull").innerHTML = "<img data-src='https://portfolio.joshua-s.website/Media/Level_3_Interactive_Media/Image/photography/Full/"+ e.target.dataset.photo + ".JPG'><p id = 'loadingIndicate'>Loading<img src = 'css/loading.svg'></p> ";
+              document.getElementById("photoFull").innerHTML += "<a href='https://portfolio.joshua-s.website/Media/Level_3_Interactive_Media/Image/photography/Full/"+ e.target.dataset.photo + ".JPG'> Full Image </a>";
+          }
 
-          document.getElementById("photoFull").innerHTML = "<img data-src='https://portfolio.joshua-s.website/Media/Level_3_Interactive_Media/Image/photography/Full/"+ e.target.dataset.photo + ".JPG'><p id = 'loadingIndicate'>Loading<img src = 'css/loading.svg'></p> ";
-          document.getElementById("photoFull").innerHTML += "<a href='https://portfolio.joshua-s.website/Media/Level_3_Interactive_Media/Image/photography/Full/"+ e.target.dataset.photo + ".JPG'> Full Image </a>";
 
           window.scrollTo(0, 0);
           lazyLoad();
